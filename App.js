a358788a596a485cdd848a9cc179c8abbdfe289b
hanzala-bhutto <bhutto.hanzala@gmail.com>
2024-05-03 20:01:32 +0500

    changed splash screen

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+
+
+// Prevent native splash screen from autohiding before App component declaration
+SplashScreen.preventAutoHideAsync()
+  .then((result) => console.log(''))
+  .catch(console.warn); // it's good to explicitly catch and inspect any error
 
 export default function App() {
+
+  useEffect(() => {
+    setTimeout(async () => {
+      await SplashScreen.hideAsync();
+    }, 4000);
+
+  }, []);
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
