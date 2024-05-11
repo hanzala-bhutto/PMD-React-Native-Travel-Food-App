7e15e8367540187c01186f3a74c0ef42097cb658
hanzala-bhutto <bhutto.hanzala@gmail.com>
2024-05-11 18:20:08 +0500

    App.js

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import TabNavigation from './app/navigation/TabNavigation';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
+import {useEffect } from 'react';
+
+import {  useFonts, Inter_200ExtraLight, Inter_300Light, Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold,  Inter_900Black } from '@expo-google-fonts/inter';
+import { ProfileContextProvider } from './app/context/profilecontext';
+
+// Prevent native splash screen from autohiding before App component declaration
+SplashScreen.preventAutoHideAsync()
+  .then((result) => console.log(''))
+  .catch(console.warn); // it's good to explicitly catch and inspect any error
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Inter_200ExtraLight, 
+    Inter_300Light,
+    Inter_400Regular, 
+    Inter_500Medium, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_800ExtraBold,
+    Inter_900Black,
+  });
+
+  useEffect(() => {
+    setTimeout(async () => {
+      await SplashScreen.hideAsync();
+    }, 4000);
+  }, []);
+
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+
+      <ProfileContextProvider>
+        <NavigationContainer>
+          <TabNavigation />
+        </NavigationContainer>
+      </ProfileContextProvider>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +54,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
